526e5ab3fb105c615b42c17d8cc971b3ae888c67
Joey Huab <joey@evolution-x.org>
2023-11-20 12:42:07 +0900

    marble: parts: Disable PocketService

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -43,7 +43,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         NfcCameraService.startService(context);
 
         // Pocket
-        PocketService.startService(context);
+        // PocketService.startService(context);
 
         // AOD
         AodBrightnessService.startService(context);
