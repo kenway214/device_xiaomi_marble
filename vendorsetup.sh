712ae2c8a4d8abbde57d08c990f9b06018cd5d63
kenway214 <kenway214@outlook.com>
2023-12-05 17:00:58 +0530

    marble: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,24 @@
+echo 'Starting to clone stuffs needed to build for marble'
+
+
+# Vendor
+echo 'Cloning vendor tree'
+rm -rf vendor/xiaomi/marble && git clone https://github.com/kenway214/vendor_xiaomi_marble.git -b derp-14 vendor/xiaomi/marble
+
+
+# Kernel
+echo 'Cloning kernel tree'
+git clone https://github.com/kenway214/device_xiaomi_marble-kernel.git -b derp-14 device/xiaomi/marble-kernel
+
+
+# Xiaomi
+echo 'Cloning hardware xiaomi'
+rm -rf hardware/xiaomi && git clone https://github.com/kenway214/hardware_xiaomi.git -b marble-14 hardware/xiaomi
+
+
+# Camera
+echo 'Cloning Leica Camera'
+git clone https://gitlab.com/marble9121630/proprietary_vendor_xiaomi_camera.git -b derp-14 vendor/xiaomi/camera
+
+
+echo 'Cloning process is completed, now its time for lunch'
